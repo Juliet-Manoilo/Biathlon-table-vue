df9875a9d7ca84d80dbf51fdc5708ebc3a538dac
juliett manoilo <mnl.juliett@gmail.com>
2021-03-28 23:37:57 +0300

    :tada: new project started

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <Table />
+  </section>
+</template>
+
+<script>
+import Table from "./components/Table.vue";
+
+export default {
+  name: "App",
+  components: { Table },
+};
+</script>
+
+<style lang="scss">
+</style>
